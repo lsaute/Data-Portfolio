0a1e01108d50c00f1e257715485f747d4edffebf
Lacy Saute <116912508+lsaute@users.noreply.github.com>
2022-11-01 13:30:10 -0500

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-select top 3
-	srvc_name,
-	min_participants
-from dbo.additional_service
-
-select top 3
-	srvc_name [additional service],
-	min_participants [minimum participants]
-from dbo.additional_service
